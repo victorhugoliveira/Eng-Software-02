51d3afa78a2ff690e2f35efa89591438218275f9
victorhugoliveira <oliveirvh@gmail.com>
2016-06-14 21:36:47 -0300

    Código

--- Implementacao/Conversor_de_Temperatura.c
+++ Implementacao/Conversor_de_Temperatura.c
@@ -2,35 +2,28 @@
 
     Autor: Victor Hugo Gomes de Oliveira
     RA: 177976
-    DescriÃ§Ã£o: Sistema destinado a converter temperatura em graus Celsius para Kelvin ou Fahrenheit.
-    MudanÃ§as Realizadas:
-                         - CriaÃ§Ã£o do "botÃ£o" Sair;
+    Descrição: Sistema destinado a converter temperatura em graus Celsius para Kelvin ou Fahrenheit.
+    Mudanças Realizadas:
+                         - Restrições de intervalos de números
                          - Melhoramento da Interface;
-                         - Melhoramento TransiÃ§Ã£o de Telas.
-    VersÃ£o: 0.2
-    Data: 23/04/2016
+                         - Otimização do Código;
+                         - Melhoramento Transição de Telas (Utilização de Funções)
 
+    Versão: 1.0
+    Data: 21/05/2016
 
 */
 
-
+// Bibliotecas utilizadas no Código
 #include<stdio.h>
 #include<stdlib.h> // Biblioteca para apagar tela no windows - system("cls");
+#include<ctype.h>
 
 
+void layout(){
 
-
-int main(){
-
-    // Ãrea de DesignaÃ§Ã£o de variÃ¡veis
-        float TempC = 0.0, TempK = 0.0, TempF = 0.0;
-        int opc = 0, selecao = 0;
-
-    // Corpo do CÃ³digo
-
-    do{     // Layout
         printf("\n\t========================================================  ");
-        printf("\n\t||  Bem-Vindo ao Software Calculadora de Temperatura  ||	");
+        printf("\n\t||    Bem-Vindo ao Sistema Conversor de Temperatura   ||	");
         printf("\n\t======================================================== ");
         printf("\n\t||                                                    ||");
         printf("\n\t||        Digite a Temperatura em Celsius             ||");
@@ -38,60 +31,110 @@ int main(){
         printf("\n\t||                                                    ||");
         printf("\n\t========================================================\n\t  ");
 
-        printf("\n\t   Temperatura em Celsius: ");
+}
+
+
+void erro_entrada_de_dado(){
+
+    printf("\n\t========================================================  ");
+    printf("\n\t||    Bem-Vindo ao Sistema Conversor de Temperatura   ||	");
+    printf("\n\t======================================================== ");
+    printf("\n\t||                                                    ||");
+    printf("\n\t||        Entre com um dos numeros: 1 ou 2            ||");
+    printf("\n\t||                                                    ||");
+    printf("\n\t||                                                    ||");
+    printf("\n\t========================================================\n\t  ");
+
+}
+
+
+void fim(){
+
+    printf("\n\t========================================================  ");
+    printf("\n\t||                 FINALIZANDO O SISTEMA ...          ||	");
+    printf("\n\t======================================================== ");
+    printf("\n\t||                                                    ||");
+    printf("\n\t||                                                    ||");
+    printf("\n\t||                         FIM                        ||");
+    printf("\n\t||                                                    ||");
+    printf("\n\t||                                                    ||");
+    printf("\n\t========================================================\n\t  ");
+
+}
+
+int main(){
+
+    // Área de Designação de variáveis
+        float TempC = 0.0, TempK = 0.0, TempF = 0.0;
+        int opc = 0, selecao = 0;
+
+    // Corpo do Código
+    do{
+        layout();               // Chamada de Função de Interface
+        printf("\n\tTemperatura em Celsius (com max 2 casas apos . ex: 25.20): ");
         scanf("%f",&TempC);                                   // Entrada de dados
 
         printf("\n\t   Digite: ");
         printf("\n\t   1 - Efetuar Calculo ");
         printf("\n\t   2 - Alterar a Temperatura ");
-        printf("\n\t   3- Sair  \n\n\t   ");        // BotÃ£o
+        printf("\n\t   3- Sair  \n\n\t   ");        // Botão
         scanf("%d",&selecao);
 
-        system("cls");                                       // Limpa Tela
+        system("cls");
 
-        if(selecao == 1){
+            switch(selecao){
 
-            // ConversÃµes de Temperatura
-            TempK = TempC + 273;
+                case 1:
+                    // Conversões de Temperatura
+                        TempK = TempC + 273.15;
 
-            TempF = (TempC * 1.8) + 32;
+                        TempF = (TempC * 1.8) + 32;
 
-            printf("\n\t========================================================  ");
-            printf("\n\t||  Bem-Vindo ao Sistema Calculadora de Temperatura   ||	");
-            printf("\n\t======================================================== ");
-            printf("\n\t||                                                    ||");
-            printf("\n\t||                                                    ||");
-            printf("\n\t||              Resultado da Conversao                ||");
-            printf("\n\t||                                                    ||");
-            printf("\n\t||                                                    ||");
-            printf("\n\t========================================================\n\t  ");
-            printf("\n\tTemperatura em Graus Kelvin: %0.2f",TempK);
-            printf("\n\tTemperatura em Graus Fahrenheit: %0.2f",TempF);
+                        printf("\n\t========================================================  ");
+                        printf("\n\t||   Bem-Vindo ao Sistema Conversor de Temperatura    ||	");
+                        printf("\n\t======================================================== ");
+                        printf("\n\t||                                                    ||");
+                        printf("\n\t||                                                    ||");
+                        printf("\n\t||              Resultado da Conversao                ||");
+                        printf("\n\t||                                                    ||");
+                        printf("\n\t||                                                    ||");
+                        printf("\n\t========================================================\n\t  ");
+                        printf("\n\tTemperatura em Graus Kelvin: %0.2f",TempK);
+                        printf("\n\tTemperatura em Graus Fahrenheit: %0.2f",TempF);
+
+                        break;
+
+                case 2:
+                    // Permite alterar Entrada de Dados
+                    opc = 1;
+                    break;
 
 
-            // CondiÃ§Ã£o para executar novamente
-            printf("\n\tDeseja efetuar nova conversao? (1)SIM ou Qualquer outro numero para NAO \n\t");   // CondiÃ§Ã£o para executar novamente
-            scanf("%d",&opc);
+                case 3:
+                    // Finaliza o Sistema
+                    fim();
+                    system("pause");
+                    system("cls");
+                    return 0;
+
+                default:
+                    // Entrada de Dados Inválida
+                    erro_entrada_de_dado();
+                    system("pause");
+                    system("cls");
+
+            }
+        // Condição para executar novamente
+        printf("\n");
+        printf("\n\tDeseja efetuar nova conversao?");
+        printf("\n\t1 SIM ou Qualquer outro numero para NAO \n\t");   // Condição para executar novamente
+        scanf("%d",&opc);
+        system("cls");
+        if(opc != 1){
+            fim();
+            system("pause");
             system("cls");
-        }
-        if(selecao == 2){
-            opc = 1;
-
-        }
-        if(selecao > 2){
-            printf("\n\t========================================================  ");
-            printf("\n\t||                 FINALIZANDO O SISTEMA ...          ||	");
-            printf("\n\t======================================================== ");
-            printf("\n\t||                                                    ||");
-            printf("\n\t||                                                    ||");
-            printf("\n\t||                         FIM                        ||");
-            printf("\n\t||                                                    ||");
-            printf("\n\t||                                                    ||");
-            printf("\n\t========================================================\n\t  ");
-            return 0;
         }
 
     }while(opc == 1);
-
-    return 0;
 }
